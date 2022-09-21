de3e6de80006421df3aef0bf7d9f82b39e6fcee1
Demo is programming <68935450+b07505054@users.noreply.github.com>
2022-09-21 13:02:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -50,8 +50,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
-    if re.match('告訴我秘密',message):
-        line_bot_api.reply_message(event.reply_token,TextSendMessage('才不告訴你哩！'))
+    
     line_bot_api.reply_message(event.reply_token,message)
 
 #主程式
