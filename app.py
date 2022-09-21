8fc0be1ff2a026e74666f895dfb433380384373d
Demo is programming <68935450+b07505054@users.noreply.github.com>
2022-09-21 18:25:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -40,8 +40,13 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
-    if text=event.message.text=="告訴我秘密":
-        line_bot_api.reply_message(event.reply_token,message)
+    message1 = TextSendMessage('hi')
+    try:
+        if text=event.message.text=="告訴我秘密":
+            
+            line_bot_api.reply_message(event.reply_token,message)
+    except:
+        line_bot_api.reply_message(event.reply_token,message1)
 
 #主程式
 import os
