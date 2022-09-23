7293c68a3651cb2973cadb7467972de58b61f0bd
Demo is programming <68935450+b07505054@users.noreply.github.com>
2022-09-23 17:36:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ line_bot_api = LineBotApi('oeV7aX9aZ0IpUw5q4xJnhgL+SRu/uvPIEg4fsaapkP3AYY3Ox+bCa
 handler = WebhookHandler('20370e7061e924b99b0ebd8078f89251')
 
 line_bot_api.push_message('Ua3077104df675bcad56981c346693b69', TextSendMessage(text='你可以開始了'))
-
+temp=0
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -40,7 +40,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
 #     if event.message.type == 'text':
-        temp=0
+        
         message = TextSendMessage(text=event.message.text)   
         if (event.message.text == '你好' or event.message.text == '您好' ) and temp==0 :
             temp=1
